6eaabe29474f85c289738319e9a5dad1a95bfab3
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-07-20 23:10:08 -0500

    added slide show in about me page

--- Assets/index.js
+++ Assets/index.js
@@ -1,9 +1,29 @@
-const movingPic = document.querySelector("#picture-size");
+var slideIndex = 0;
+showSlides();
 
-movingPic.addEventListener("click", (e) => {
-  e.preventDefault();
-  mediumZoom("#picture-size");
-});
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); 
+}
+
+// const movingPic = document.querySelector("#picture-size");
+
+// movingPic.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   mediumZoom("#picture-size");
+// });
 
 // var slideimages = new Array() // create new array to preload images
 // slideimages[0] = new Image() // create new instance of image object
